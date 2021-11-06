163f63a43a4ce6f8e9cb6d0f87100a14e09b137c
Diego Ramos <diego_ramos93@outlook.com>
2021-11-06 19:16:03 -0300

    ajuste calculo altura

--- calc.js
+++ calc.js
@@ -1,18 +1,21 @@
 
 		
 
-		var js_peso;
-		var js_altura;
-		var js_imc;
-        var js_resultado;
+		let js_peso;
+		let js_altura;
+		let js_imc;
+        let js_resultado;
         
 
 		function calcular() {
 		
 		js_peso= document.imcdados.f_peso.value;
 		js_altura= document.imcdados.f_altura.value;
-		js_imc= js_peso / (js_altura ^ 2);
+		js_imc= js_peso / (js_altura / 100 ^ 2);
         
         window.alert('Seu IMC é: ' + js_imc);
 }
+
+  
+  
 
