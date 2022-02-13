cbfb8ef5cf3059ddb01272c1737f14965fe74db6
Diego Ramos <diego_ramos93@outlook.com>
2022-02-13 11:56:00 -0300

    ajuste no javascript

--- src/controller/calc.js
+++ src/controller/calc.js
@@ -1,18 +1,17 @@
-var js_peso;
-var js_altura;
-var js_imc;	
 
 		  calculate = () => {
 
-		js_peso= document.imcdados.f_peso.value;
-		js_altura= document.imcdados.f_altura.value;
-		js_imc= js_peso / (js_altura / 100 ^ 2);
+		let js_peso= document.imcdados.f_peso.value;
+		let js_altura= document.imcdados.f_altura.value;
+		let js_imc= js_peso / (js_altura / 100 ^ 2);
+
 		let result = document.querySelector(".imcResult");
 		
 		result.innerHTML = ("Seu IMC É: " + js_imc.toFixed(2));
 
 		 }
 
+		 //Press Enter for calculate
 		 document.addEventListener('keypress', e=>{
 
 			let btn = document.querySelector('#btnCa');
